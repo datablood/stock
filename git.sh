6a82c53cbaa7b0cdab20f2a36f0478909f438beb
pang <pangyexing@qfpay.com>
2016-03-24 18:06:08 +0800

    delete unused filewq

--- git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "# stock" >> README.md
-git init
-git add README.md
-git commit -m "datablood commit"
-git remote add origin git@github.com:datablood/stock.git
-git push -u origin master
