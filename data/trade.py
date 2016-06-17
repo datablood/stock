92d08ceff2efe402c58fc79358d55d6267750114
pang <pangyexing@qfpay.com>
2016-06-18 06:53:29 +0800

    修改生成器bug

--- data/trade.py
+++ data/trade.py
@@ -78,7 +78,8 @@ def get_hist6years(split=0.2,
                 Y_train.append(temp_y)
         k += 1
         samples = 5
-        if ((k % samples) & ((len(stockcodes) - k) > samples)) == 0:
+        if k % samples == 0:
+            print k
             log.info('%s stock finished ', k)
             yield ((np.asarray(X_train), np.asarray(Y_train),
                     np.asarray(ID_train)),
